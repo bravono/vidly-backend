58288e6555da20c1040c8da969a8c18a2ddfca90
Ahbideen Yusuf <ahbideenyusuf@gamil.com>
2023-12-25 15:44:30 +0100

    Build the rentals API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const genres = require("./routes/genres");
 const home = require("./routes/home");
 const customers = require(".routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 const express = require("express");
 const app = express();
 
@@ -16,6 +17,7 @@ app.use("/api/genres", genres);
 app.use("/", home);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 5000;
 app.listen(5000, () => {
---
+++ models/rental.js
@@ -0,0 +1,74 @@
+const Joi = require("joi");
+const date = require("joi/lib/types/date");
+const mongoose = require("mongoose");
+
+const rentalSchema = new mongoose.Schema({
+  custome: {
+    type: new mongoose.schema({
+      name: {
+        type: String,
+        trim: true,
+        minlength: 5,
+        maxlength: 255,
+        required: true,
+      },
+
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: Number,
+        minlength: 5,
+        maxlength: 50,
+        required: true,
+      },
+    }),
+  },
+
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        trim: true,
+        minlength: 5,
+        maxlength: 50,
+        required: true,
+      },
+
+      dailyRentalRate: {
+        type: Number,
+        min: 0,
+        max: 255,
+        required: true,
+      },
+    }),
+  },
+
+  dateOut: {
+    type: Date,
+    default: Date.now,
+  },
+
+  dateReturned: {
+    type: Date,
+  },
+
+  retntalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+const Rental = mongoose.model("Rental", rentalSchema);
+
+function validateRental(rental) {
+  const schema = {
+    customerId: Joi.String().required(),
+    movieId: Joi.String().required(),
+  };
+
+  return Joi.validate(rental, schema);
+}
+
+exports.Rental = Rental;
+exports.validateRental = validate;
