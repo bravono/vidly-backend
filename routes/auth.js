7194b7e34407841d119cab299ebe26b9e0da0075
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-01-02 17:55:25 +0100

    Authenticate user

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs");
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne(req.body.email);
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  res.send(true);
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(50).required().email(),
+    password: Joi.string().min(5).max(50).required(),
+  };
+}
+
+module.exports = router;
