de9e66af93c5d91d470058297a44365e95df8133
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-01-04 17:35:33 +0100

    Encapsulate jwt logic

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
@@ -44,6 +46,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+};
 const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
