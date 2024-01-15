d5f288438ec895230b0d51196b98a24d844297fb
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-01-15 13:45:04 +0100

    Extract routes to a separate module

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 require("dotenv").config();
 const winston = require("winston");
 const { MongoDB } = require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const home = require("./routes/home");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const Auth = require("./routes/auth");
+
 const express = require("express");
 const app = express();
 
-process.on("uncaughtException", (ex) => {
-  console.log("WE GOT AN UNCAUGHT EXCEPTION");
-  winston.error(ex.message, ex);
+require("./startup/routes")(app);
+
+winston.handleExceptions(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" })
+);
+
+process.on("unhandledRejection", (ex) => {
+  throw ex;
 });
 
 winston.add(winston.transports.File, { filename: "logfile.log" });
@@ -36,8 +34,6 @@ const logger = new winston.Logger({
   level: "error  ", // Only log errors
 });
 
-throw new Error("Something failed during startup");
-
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivatekey is not defined");
   process.exit(1);
@@ -47,16 +43,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Could not connect to MongoDB"));
-
-app.use(express.json());
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/rentals", rentals);
-app.use("/api/movies", movies);
-app.use("/api/users", users);
-app.use("/api/auth", Auth);
-app.use(error);
 
 const port = process.env.PORT || 5000;
 app.listen(5000, () => {
