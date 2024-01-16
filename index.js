e16e122284c1ddf4ea1025b9dcd06285dd35be60
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-01-16 11:34:36 +0100

    Extract config logic

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
-const config = require("config");
 const express = require("express");
 const app = express();
 
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/database")();
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivatekey is not defined");
-  process.exit(1);
-}
+require("./startup/config")();
 
 const port = process.env.PORT || 5000;
 app.listen(5000, () => {
