830b48f59034beda9887fe324a63385f6baf8b22
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-01-16 11:20:00 +0100

    Extract logging logic

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
 require("dotenv").config();
-const winston = require("winston");
-const { MongoDB } = require("winston-mongodb");
 const config = require("config");
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/database")();
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-// winston.add(winston.transports.MongoDB, {
-//   db: "mongodb://loclahost/vidly",
-//   collection: "log",
-// });
-
-const logger = new winston.Logger({
-  transports: [
-    new MongoDB({
-      db: "mongodb://localhost/vidly",
-      collection: "log",
-      // Other MongoDB connection options if needed
-    }),
-  ],
-  level: "error  ", // Only log errors
-});
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivatekey is not defined");
---
+++ startup/logging.js
@@ -0,0 +1,30 @@
+const winston = require("winston");
+const { MongoDB } = require("winston-mongodb");
+require("express-async-errors");
+
+module.exports = function () {
+  winston.handleExceptions(
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  process.on("unhandledRejection", (ex) => {
+    throw ex;
+  });
+
+  winston.add(winston.transports.File, { filename: "logfile.log" });
+  // winston.add(winston.transports.MongoDB, {
+  //   db: "mongodb://loclahost/vidly",
+  //   collection: "log",
+  // });
+
+  const logger = new winston.Logger({
+    transports: [
+      new MongoDB({
+        db: "mongodb://localhost/vidly",
+        collection: "log",
+        // Other MongoDB connection options if needed
+      }),
+    ],
+    level: "error  ", // Only log errors
+  });
+};
