e3655ec48d511b3251e3f7fd864287236dd9c00d
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-01-27 10:16:48 +0100

    Write test to get list of genres

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const winston = require("winston");
 const express = require("express");
 const app = express();
 
-require("./startup/logging")();
+require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/database")();
 require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   winston.info(`Listening on port ${port}...`);
 });
+
+module.exports = server;
