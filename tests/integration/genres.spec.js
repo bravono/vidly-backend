e297f5991f25b54fa192f5332481cf2532464f41
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-02-19 12:55:42 +0100

    Test routes with params

--- tests/integration/genres.spec.js
+++ tests/integration/genres.spec.js
@@ -1,3 +1,5 @@
+const { describe } = require("joi/lib/types/lazy");
+const { Genre } = require("../../models/genre");
 const request = require("supertest");
 let server;
 
@@ -10,8 +12,28 @@ describe("/api/genres", () => {
   });
   describe("GET /", () => {
     it("should get the list of genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+        { name: "genre3" },
+      ]);
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+
+      await Genre.remove({});
+    });
+  });
+
+  describe("GET/:id", async () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
     });
   });
 });
