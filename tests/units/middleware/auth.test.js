d7e403bd7ce4dcedc85d9d181b17c1a4fb44ae00
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2024-02-29 08:08:09 +0100

    Test auth middleware specific value

--- tests/units/middleware/auth.test.js
+++ tests/units/middleware/auth.test.js
@@ -1,14 +1,19 @@
 const auth = require("../../../middleware/auth");
 const { User } = require("../../../models/user");
+const mongoose = require("mongoose");
 
 describe("auth middleware", () => {
   it("should populate req.user with the payload of a valid JWT", () => {
-    const token = new User().generateAuthToken();
+    const user = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const token = new User(user).generateAuthToken();
     const res = {};
     const next = jest.fn();
     const req = { header: jest.fn().mockReturnValue(token) };
     auth(req, res, next);
 
-    expect(req.user).toBeDefined();
+    expect(req.user).toMatchObject(user);
   });
 });
